84ae69e894dedc6bcba4db12bf72c3cac5aff8a8
Adiitya786 <adityaprajapatii786@gmail.com>
2025-07-24 09:57:02 +0530

    Basic understanding of syntax of functions in java.

---
+++ Functions/Methods/cncptFunction.java
@@ -0,0 +1,13 @@
+
+public class cncptFunction{
+
+   public static void printname(){
+        System.out.println("Aditya Prajapati");
+        System.out.println("Aditya Prajapati");
+        System.out.println("Aditya Prajapati");
+        return;
+    }
+    public static void main(String[] args) {
+        printname();      //    Function call
+    }
+}
