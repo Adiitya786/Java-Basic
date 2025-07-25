25c459fb662cfbb2d9623fa742350c099ec5b5cd
Adiitya786 <adityaprajapatii786@gmail.com>
2025-07-25 10:12:58 +0530

    Using function overloading, claculating sum of either three no.s or four no.s.

--- Functions/sum.java
+++ Functions/sum.java
@@ -1,17 +1,21 @@
  import java.util.*;
  public class sum {
+    // to calculate sum of three numbers.
     public static int calculateSum(int num1,int num2 ,int num3){
-        int sum = num1 + num2+ num3;
-        return sum;
+        return num1 + num2+ num3;
+    }
+    public static int calculateSum(int num1,int num2 ,int num3,int num4){
+        return num1+num2+num3+num4;
     }
     public static void main(String[] args) {
         System.out.println("enter the value of three operands:");
         Scanner sc  = new Scanner(System.in);
-        int a = sc.nextInt();
+        /*  int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int sum =  calculateSum(a,b,c);
-        System.out.println("sum is:"+ sum);
+        int d = sc.nextInt();*/
+        System.out.println("sum is:"+ calculateSum(3,4,5));
+        System.out.println("sum is:"+ calculateSum(1,2,4,5));
         sc.close();
     }
     
